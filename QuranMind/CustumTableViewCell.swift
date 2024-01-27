57ec54e94a5d444845c636432fa9f2041c50e7d8
AdhamRaouf <adhamraouf777@gmail.com>
2024-01-27 02:53:55 +0200

    first commit

---
+++ QuranMind/CustumTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustumTableViewCell.swift
+//  QuranMind
+//
+//  Created by Adham Raouf on 27/01/2024.
+//
+
+import UIKit
+
+class CustumTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
